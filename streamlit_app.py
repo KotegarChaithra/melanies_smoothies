152803c64dd5a4491547da3c6162b1f41057cd62
KotegarChaithra <155987898+KotegarChaithra@users.noreply.github.com>
2024-04-13 19:34:19 +0530

    Update streamlit_app.py
    addedd .json() to response code

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ ingredients_list=st.multiselect(
 )
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 if ingredients_list:
 
     ingredients_string = ''
